866cf4a6cba7f89c46126340d3b688e975da133f
Nanase-Iso <nanase20@users.noreply.github.com>
2020-11-06 16:52:50 +0900

    課題提出

--- JavaScript/2-1/main.js
+++
@@ -1,7 +0,0 @@
-// 「myfunc」という関数を用意。
-function myfunc() {
-    // idが「box」の要素を取得して、変数changeに代入
-    let change = document.getElementById("box");
-    // change.style.backgroundColorを使って色を書き変える
-    change.style.backgroundColor = "red";
-}
